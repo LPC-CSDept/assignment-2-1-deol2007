ea0125ed0450af07c6cdcce63c1240150dc62c28
deol2007 <deolsarika9@gmail.com>
2023-06-23 12:32:29 -0700

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 def main():
     """
     ##################################################
-    Comlete your code here
+    Complete your code here
     Use m_perc and f_perc for your results
     ##################################################
     """
+    
+    num_male = int(input("Enter number of males:"))
+    num_female =int(input("Enter number of females:"))
 
+    total_people = num_male + num_female
+
+    m_perc = num_male/total_people * 100
+    f_perc = num_female/total_people * 100
+
+    print(f"Total Number of People:{total_people}")
+    print(f"Percent of Males:{m_perc:.2f}")
+    print(f"Percent of Female:{f_perc:.2f}")
     """
     ########################################
     # Do not delete the return statement

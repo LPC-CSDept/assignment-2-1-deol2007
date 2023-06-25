ee07895fb630ee6629178bd9ebcc67138646b0bd
deol2007 <deolsarika9@gmail.com>
2023-06-24 17:11:46 -0700

    Stage

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def main():
     f_perc = num_female/total_people * 100
 
     print(f"Total Number of People:{total_people}")
-    print(f"Percent of Males:{m_perc:.2f}")
-    print(f"Percent of Female:{f_perc:.2f}")
+    print(f"Number of Males and Females: {num_male:.2f} \t {num_female:.2f}")
+    print(f"Percent of Males and Females: {m_perc:.2f} \t {f_perc:.2f}")
     """
     ########################################
     # Do not delete the return statement
